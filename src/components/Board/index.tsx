c23e263310085cc11e2b4e7f35fd4dac5bfd2181
v.belousov <vladislav0151@bk.ru>
2023-06-10 00:30:17 +0300

    feat(board): add pieces

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import styles from './assets/styles/Board.module.scss'
 
+// I'm gonna import classnames soon but now im too lazy
+const cx = (...classes: string[]) => {
+    return classes.map(el => styles[el]).join(' ');
+}
+
 const Board = () => {
     return (
         <div className={styles.board}>
             {[...Array(8).keys()].map((row) =>
                 <div key={row} className={styles.row}>
-                    {[...Array(8).keys()].map((cell) => <div key={cell} className={styles.cell}></div>)}
+                    {[...Array(8).keys()].map((cell) => <div key={cell} className={cx('cell', 'piece', 'white')}></div>)}
                 </div>
             )}
         </div>
